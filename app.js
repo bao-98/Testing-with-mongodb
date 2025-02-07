d1fc44aef0dcc6a2a337babfaf99a207418b6b1b
Dinh Quoc Bao Duong <dduong02@mylangara.ca>
2025-02-06 18:34:59 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-import express from "express"
+import express from 'express';
+import {client, connection} from './database.js';
+
 const app = express();
+
 let server;
-
-import {client, connection} from "./database.js";
-
 connection.then(()=>{
-    console.log("Successful connection to database!");
-    server = app.listen(3000, () =>console.log('Server listening.'));
+  console.log("Successful connection to database!");
+  server = app.listen(3000, ()=>console.log('Server listening.'));
 })
 .catch(e=>console.error(e));
+
+// Create Database
 const database = client.db('our-first-database');
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req,res)=>{
+    res.send(`Hello!`);
+});
+
 process.on('SIGINT', ()=>{
     client.close();
-    console.log("closed database connection");
+    console.log("closed database connection"); 
     process.exit(1);
-})
+});
