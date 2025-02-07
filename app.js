1061d8ae40ca4d9c7e9add08ad0989a133a8454f
Dinh Quoc Bao Duong <dduong02@mylangara.ca>
2025-02-07 02:17:50 +0000

    mongodb testing

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 import express from "express"
 const app = express();
 let server;
+
+import {client, connection} from "./database.js";
+
+connection.then(()=>{
+    console.log("Successful connection to database!");
+    server = app.listen(3000, () =>console.log('Server listening.'));
+})
+.catch(e=>console.error(e));
+const database = client.db('our-first-database');
+process.on('SIGINT', ()=>{
+    client.close();
+    console.log("closed database connection");
+    process.exit(1);
+})
